08881d86227a7e3c64721f4c1e4a330aee59613f
vagrant <vagrant@homestead>
2018-12-28 08:52:57 +0000

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,4 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::resource('users', 'UsersController', ['only'=>['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController');
+Route::resource('categories', 'CategoriesController', ['only'=>'show']);
